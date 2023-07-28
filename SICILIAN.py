28d044ce7ebd8b18ff81051c0405db720ec3096a
Timothy Wu <thsuanwu@stanford.edu>
2023-07-28 16:37:03 +0800

    Update SICILIAN.py

--- SICILIAN.py
+++ SICILIAN.py
@@ -118,6 +118,7 @@ def class_input(out_path, name, gtf_file, annotator_file, tenX, single, stranded
 
 def STAR_map(out_path, data_path, name, r_ends, gzip, single, gtf_file, tenX, star_path, star_ref_path, dep = ""):
   """Run script to perform mapping job for STAR"""
+  print(data_path)
   print(name)
   # Pattern matching to find R1 and R2 files
   r1_files = glob.glob(os.path.join(data_path, f"{name}*R1*fastq*"))
