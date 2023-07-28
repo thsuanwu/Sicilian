8681fdcc21b28e7bae80a5c70fec6ef4f80c31ff
Timothy Wu <thsuanwu@stanford.edu>
2023-07-28 16:29:36 +0800

    Update SICILIAN.py

--- SICILIAN.py
+++ SICILIAN.py
@@ -116,7 +116,7 @@ def class_input(out_path, name, gtf_file, annotator_file, tenX, single, stranded
   sbatch_file("run_class_input.sh", out_path, name,"class_input_{}".format(name), "48:00:00", "200Gb", command, dep=dep)  # 96:00:00, and 210 Gb for Lu, 100 for others
   return submit_job("run_class_input.sh")
 
-def STAR_map(out_path, data_path, name, gzip, single, gtf_file, tenX, star_path, star_ref_path, suffix=".fastq.gz", dep=""):
+def STAR_map(out_path, data_path, name, r_ends, gzip, single, gtf_file, tenX, star_path, star_ref_path, dep = ""):
   """Run script to perform mapping job for STAR"""
   print(single)
   # Pattern matching to find R1 and R2 files
@@ -357,7 +357,6 @@ def main():
       job_nums.append(extract_jobid)
     else:
       extract_jobid = ""
-    print(single)
     if run_map:
       map_jobid = STAR_map(project_path, data_path, name, r_ends, gzip, single, gtf_file, tenX, star_path, star_ref_path, dep = ":".join(job_nums))
       jobs.append("map_{}.{}".format(name,map_jobid))
